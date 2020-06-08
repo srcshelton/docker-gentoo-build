79a36ff33473050b21444fbda1dd2635eb81900a
Stuart Shelton <stuart@shelton.me>
2020-06-08 22:45:38 +0000

    Add podman support to show-vars.sh
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- common/show-vars.sh
+++ common/show-vars.sh
@@ -10,10 +10,14 @@ elif echo " ${*:-} " | grep -Eq -- ' -(h|-help) '; then
 	exit 0
 fi
 
+if type -pf podman >/dev/null 2>&1; then
+	docker='podman'
+fi
+
 # The 'inspect' command works with containers (and container IDs) too...
-if [ "$( docker image ls "${image}" | wc -l )" != '2' ]; then
+if [ "$( "${docker}" image ls "${image}" | wc -l )" != '2' ]; then
 	echo >&2 "WARN:  Cannot determine unique image '${image}'"
 	#exit 1
 fi
 
-docker inspect -f '{{ .Config.Env }}' "${image}" | tr $'\t' ' ' | tr -s '[:space:]' | sed -r 's/^\[(.*)\]$/\1/' | sed -r 's/ ([A-Za-z][A-Za-z0-9_-]*)=/\n\1=/g'
+"${docker}" inspect -f '{{ .Config.Env }}' "${image}" | tr $'\t' ' ' | tr -s '[:space:]' | sed -r 's/^\[(.*)\]$/\1/' | sed -r 's/ ([A-Za-z][A-Za-z0-9_-]*)=/\n\1=/g'
