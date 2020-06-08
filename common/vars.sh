94b7a3752fc3fe0649df6ecb13bd73dc2894d456
Stuart Shelton <stuart@shelton.me>
2020-06-08 22:48:41 +0000

    Add podman support to vars.sh
     * Define default 'use_essential' USE flags
    
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- common/vars.sh
+++ common/vars.sh
@@ -11,15 +11,27 @@ build_name="gentoo-build"
 #
 environment_filter='^(declare -x|export) (EDITOR|GENTOO_PROFILE|HOME|HOSTNAME|LESS(OPEN)?LS_COLORS|(MAN)?PAGER|(OLD)?PWD|PATH|(|SYS|PORTAGE_CONFIG)ROOT|SHLVL|TERM)='
 
+# Define essential USE flags
+#
+# dev-libs/openssl: asm tls-heartbeat zlib
+# sys-apps/busybox: mdev
+use_essential="asm mdev tls-heartbeat zlib"
+
 # Colour options!
 #
-reset=$'\e[0m'
 bold=$'\e[1m'
 red=$'\e[31m'
 green=$'\e[32m'
 blue=$'\e[34m'
+# Place 'reset' last to prevent coloured xtrace output!
+reset=$'\e[0m'
 
 # Export portage job-control variables...
 #
 export JOBS='4'
 export MAXLOAD='4.0'
+
+# Are we using docker or podman?
+docker='docker'
+#extra_build_args=''
+docker_readonly='readonly'
