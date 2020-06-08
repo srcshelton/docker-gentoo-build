b1b825580b979ab3ddbf3d7c0f4db9a4b6e708aa
Stuart Shelton <stuart@shelton.me>
2020-06-08 22:57:56 +0000

    Add podman support to prune.sh
     * FIXME: Forced-removal of image can break image associations in libpod-1.9.3?
    
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- common/prune.sh
+++ common/prune.sh
@@ -1,9 +1,23 @@
 #! /bin/sh
 
-set -e
+set -eu
 
-docker ps -a | tr -cd '[[:print:]\n]' | rev | cut -d' ' -f 1 | rev | grep '_' | xargs -r docker rm
+trace=${TRACE:-}
 
-docker image ls | grep '^<none>\s\+<none>' | awk '{ print $3 }' | xargs -r docker image rm
+[ -n "${trace:-}" ] && set -x
 
-docker image ls --filter 'dangling=true' | tail -n +2 | awk '{ print $3 }' | xargs -r docker image rm
+docker='docker'
+if type -pf podman >/dev/null 2>&1; then
+	docker='podman'
+fi
+
+$docker ps -a | tr -cd '[[:print:]\n]' | rev | cut -d' ' -f 1 | rev | grep '^[a-z]_[a-z]$' | xargs -r $docker rm
+
+$docker image ls | grep '^<none>\s\+<none>' | awk '{ print $3 }' | xargs -r $docker image rm || :
+#while $docker image ls | grep -q '^<none>\s\+<none>'; do
+#	$docker image ls | grep '^<none>\s\+<none>' | awk '{ print $3 }' | xargs -r $docker image rm -f || :
+#done
+
+$docker image ls --filter 'dangling=true' | tail -n +2 | awk '{ print $3 }' | xargs -r $docker image rm
+
+set +x
