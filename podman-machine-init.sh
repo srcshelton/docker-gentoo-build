675d89c433ab8339e2571f10375636b5ca7b835c
Stuart Shelton <stuart@shelton.me>
2024-02-24 14:33:34 +0000

    Add '--host' option to allow deployment on to non-Gentoo hosts
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- podman-machine-init.sh
+++ podman-machine-init.sh
@@ -6,7 +6,9 @@ export LANG=C
 export LC_ALL=C
 
 declare MACHINE='podman'
-declare -i init=0 xfer=0 cores=4
+declare REMOTE_HOME='/var/home'
+declare REMOTE_USER='core'
+declare -i init=0 xfer=0 cores=4 local_install=0
 
 if [[ "$( uname -s )" == 'Darwin' ]]; then
 	readlink() {
@@ -24,7 +26,7 @@ if [[ "$( uname -s )" == 'Darwin' ]]; then
 			cores="$( sysctl -n machdep.cpu.core_count )" ;;
 	esac
 else
-	cores="$( grep 'cpu cores' /proc/cpuinfo | tail -n 1 | awk -F': ' '{ print $2 }' )"
+	cores="$( nproc || grep 'cpu cores' /proc/cpuinfo | tail -n 1 | awk -F': ' '{ print $2 }' )"
 fi
 cd "$( dirname "$( readlink -e "${0}" )" )" || exit 1
 
@@ -32,11 +34,18 @@ declare arg=''
 for arg in "${@:-}"; do
 	case "${arg:-}" in
 		-h|--help)
-			echo "Usage: $( basename "${0}" ) [--machine=<name>] [--cores=<${cores}>] [--init] [--transfer-cache]"
+			echo "Usage: $( basename "${0}" ) <--host>|[--machine=<name>] [--cores=<${cores}>] [--init] [--transfer-cache]"
 			exit 0
 			;;
 		-c|--cores)
 			cores="${arg#*"-"}"
+			;;
+		--host)
+			if ! [[ "${*:-}" == '--host' ]]; then
+				echo >&2 "FATAL: '--host' cannot be used with any other options"
+				exit 1
+			fi
+			local_install=1
 			;;
 		-i|--init)
 			init=1
@@ -51,71 +60,90 @@ for arg in "${@:-}"; do
 done
 unset arg
 
-if ! [[ -f ~/.ssh/id_ed25519 && -s ~/.ssh/id_ed25519 ]]; then
-	echo >&2 "FATAL: ssh private key '${HOME}/.ssh/id_ed25519' not accessible"
-	exit 1
-fi
+if (( local_install )); then
+	sudo mkdir -p /var/cache &&
+		test -s ${REMOTE_HOME}/${REMOTE_USER}/portage-cache.tar &&
+		sudo tar -xpf ${REMOTE_HOME}/${REMOTE_USER}/portage-cache.tar -C /var/cache/
 
-if ! podman machine list | grep -q -- "^${MACHINE}"; then
-	podman machine init --cpus "${cores}" --disk-size 25 -m $(( 12 * 1024 )) "${MACHINE}"
-fi
-if ! podman machine list | grep -q -- "^${MACHINE}.*Currently running"; then
-	podman machine start "${MACHINE}"
-	until [[ "$( podman machine list --noheading --format '{{.Name}} {{.LastUp}}' | grep "^${MACHINE}[* ]" )" =~ ^${MACHINE}\*?\ Currently\ running$ ]]; do
-		printf '.'
-		sleep 0.1
-	done
-	until podman machine ssh "${MACHINE}" 'true'; do
-		printf '.'
-		sleep 0.1
-	done
-	echo
-fi
+	sudo mkdir -p /var/cache/portage &&
+		sudo chown ${REMOTE_USER}:root /var/cache/portage &&
+		sudo chmod ug+rwX /var/cache/portage
 
-if [[ -s ~/.ssh/authorized_keys ]] && grep -Fq -- "$( < ~/.ssh/"${MACHINE}.pub" )" ~/.ssh/authorized_keys; then
-	:
-else
-	mkdir -p ~/.ssh
-	chmod 0700 ~/.ssh
-	cat ~/.ssh/"${MACHINE}.pub" >> ~/.ssh/authorized_keys
-	chmod 0600 ~/.ssh/authorized_keys
-fi
+	if ! [[ -x sync-portage.sh ]]; then
+		echo >&2 "WARN:  Cannot locate 'sync-portage.sh' script - please run" \
+			"this manually in order to populate '/etc/portage'"
+	else
+		sudo ./sync-portage.sh
 
-podman machine ssh "${MACHINE}" 'cat - > ~/.ssh/id_ed25519 && chmod 0600 ~/.ssh/id_ed25519' < ~/.ssh/"${MACHINE}"
-podman machine ssh "${MACHINE}" 'cat - > ~/.ssh/id_ed25519.pub && chmod 0600 ~/.ssh/id_ed25519.pub' < ~/.ssh/"${MACHINE}.pub"
-
-podman machine ssh "${MACHINE}" <<EOF
-	test -d src/docker-gentoo-build || {
-		mkdir -p src &&
-		cd src &&
-		git clone https://github.com/srcshelton/docker-gentoo-build.git ;
-	} ;
-	git config --system --replace-all safe.directory '*' ;
-	cd ~/src/docker-gentoo-build && git pull --all
-EOF
-
-for f in 'local.sh' 'portage-cache.tar'; do
-	if [[ -f "${f}" && -s "${f}" ]]; then
-		case "${f}" in
-			*.sh)
-				dest="/var/home/core/src/docker-gentoo-build/common/" ;;
-			*.tar)
-				dest="/var/home/core/" ;;
-		esac
-		podman machine ssh "${MACHINE}" "scp -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no -v '$( id -nu )@$( hostname -s ).seventytwo.miltonroad.net:$( pwd )/${f}' '${dest}'"
+		echo >&2 "INFO:  Please review the settings in '/etc/portage/make.conf'"
 	fi
-done
-
-podman machine ssh "${MACHINE}" 'sudo mkdir -p /var/cache ; test -s /var/home/core/portage-cache.tar && sudo tar -xpf /var/home/core/portage-cache.tar -C /var/cache/ || sudo mkdir -p /var/cache/portage ; sudo chown core:root /var/cache/portage && sudo chmod ug+rwX /var/cache/portage'
-
-if ! (( init || xfer )); then
-	podman machine ssh "${MACHINE}"
 else
-	if (( init )); then
-		time podman machine ssh "${MACHINE}" '/var/home/core/src/docker-gentoo-build/gentoo-init.docker'
+	if ! [[ -f ~/.ssh/id_ed25519 && -s ~/.ssh/id_ed25519 ]]; then
+		echo >&2 "FATAL: ssh private key '${HOME}/.ssh/id_ed25519' not accessible"
+		exit 1
 	fi
-	if (( xfer )); then
-		podman machine ssh "${MACHINE}" 'cd /var/cache && tar -cvpf - portage' > portage-cache.tar
+
+	if ! podman machine list | grep -q -- "^${MACHINE}"; then
+		podman machine init --cpus "${cores}" --disk-size 25 -m $(( 12 * 1024 )) "${MACHINE}"
+	fi
+	if ! podman machine list | grep -q -- "^${MACHINE}.*Currently running"; then
+		podman machine start "${MACHINE}"
+		until [[ "$( podman machine list --noheading --format '{{.Name}} {{.LastUp}}' | grep "^${MACHINE}[* ]" )" =~ ^${MACHINE}\*?\ Currently\ running$ ]]; do
+			printf '.'
+			sleep 0.1
+		done
+		until podman machine ssh "${MACHINE}" 'true'; do
+			printf '.'
+			sleep 0.1
+		done
+		echo
+	fi
+
+	if [[ -s ~/.ssh/authorized_keys ]] && grep -Fq -- "$( < ~/.ssh/"${MACHINE}.pub" )" ~/.ssh/authorized_keys; then
+		:
+	else
+		mkdir -p ~/.ssh
+		chmod 0700 ~/.ssh
+		cat ~/.ssh/"${MACHINE}.pub" >> ~/.ssh/authorized_keys
+		chmod 0600 ~/.ssh/authorized_keys
+	fi
+
+	podman machine ssh "${MACHINE}" 'cat - > ~/.ssh/id_ed25519 && chmod 0600 ~/.ssh/id_ed25519' < ~/.ssh/"${MACHINE}"
+	podman machine ssh "${MACHINE}" 'cat - > ~/.ssh/id_ed25519.pub && chmod 0600 ~/.ssh/id_ed25519.pub' < ~/.ssh/"${MACHINE}.pub"
+
+	podman machine ssh "${MACHINE}" <<-EOF
+		test -d src/docker-gentoo-build || {
+			mkdir -p src &&
+			cd src &&
+			git clone https://github.com/srcshelton/docker-gentoo-build.git ;
+		} ;
+		git config --system --replace-all safe.directory '*' ;
+		cd ~/src/docker-gentoo-build && git pull --all
+	EOF
+
+	for f in 'local.sh' 'portage-cache.tar'; do
+		if [[ -f "${f}" && -s "${f}" ]]; then
+			case "${f}" in
+				*.sh)
+					dest="${REMOTE_HOME}/${REMOTE_USER}/src/docker-gentoo-build/common/" ;;
+				*.tar)
+					dest="${REMOTE_HOME}/${REMOTE_USER}/" ;;
+			esac
+			podman machine ssh "${MACHINE}" "scp -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no -v '$( id -nu )@$( hostname -s ).seventytwo.miltonroad.net:$( pwd )/${f}' '${dest}'"
+		fi
+	done
+
+	podman machine ssh "${MACHINE}" "sudo mkdir -p /var/cache ; test -s ${REMOTE_HOME}/${REMOTE_USER}/portage-cache.tar && sudo tar -xpf ${REMOTE_HOME}/${REMOTE_USER}/portage-cache.tar -C /var/cache/ || sudo mkdir -p /var/cache/portage ; sudo chown ${REMOTE_USER}:root /var/cache/portage && sudo chmod ug+rwX /var/cache/portage"
+
+	if ! (( init || xfer )); then
+		podman machine ssh "${MACHINE}"
+	else
+		if (( init )); then
+			time podman machine ssh "${MACHINE}" "${REMOTE_HOME}/${REMOTE_USER}/src/docker-gentoo-build/gentoo-init.docker"
+		fi
+		if (( xfer )); then
+			podman machine ssh "${MACHINE}" 'cd /var/cache && tar -cvpf - portage' > portage-cache.tar
+		fi
 	fi
 fi
 
