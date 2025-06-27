srcshelton/docker-gentoo-build

commit 814e3ed39263bb5d574c9a40725fd174fb88ea86

Change default PKGHOST value from 'docker' to 'container'
Standardise use of 'grep' and 'awk'

Signed-off-by: Stuart Shelton <stuart@shelton.me>

common/ls-old-images.sh
@@ -49,7 +49,7 @@ echo "${images}" |
 	grep --colour=always '^localhost/gentoo-build.*$'
 
 echo "${images}" |
-	grep '^localhost/gentoo-build' -A "${lines:-"100"}" |
+	grep -- '^localhost/gentoo-build' -A "${lines:-"100"}" |
 	grep -v -e '^localhost/gentoo-\(build\|base\|init\|stage3\|env\)' -e '^docker.io/gentoo/stage3' |
 	grep --colour=never '^localhost/\(service\|sys-kernel\.\)'
 

common/cpu_flags.sh
@@ -4,7 +4,7 @@ set -u
 
 debug="${DEBUG:-"0"}"
 
-def_repo="$( portageq repositories_configuration / | grep -m 1 '^\[DEFAULT\]$' -A 64 | grep -m 1 '^$' -B 64 | grep '^main-repo = ' | cut -d'=' -f 2- | xargs )"
+def_repo="$( portageq repositories_configuration / | grep -m 1 '^\[DEFAULT\]$' -A 64 | grep -m 1 '^$' -B 64 | grep -- '^main-repo = ' | cut -d'=' -f 2- | xargs )"
 if [ -z "${def_repo:-}" ]; then
 	echo >&2 "FATAL: Cannot read DEFAULT portage repo"
 	exit 1
@@ -34,12 +34,12 @@ fi
 while read -r line; do
 	echo "${line}" | grep -q -- ' - ' || continue
 
-	flag="$( echo "${line}" | awk -F' - ' '{ print $1 }' )"
+	flag="$( echo "${line}" | awk -F' - ' '{print $1}' )"
 	[ $(( debug )) -eq 1 ] && echo >&2 "DEBUG: Found description for flag '${flag}' ($line)"
-	if echo "${line}" | grep "^${flag} - " | grep -Fq -- '[' ; then
+	if echo "${line}" | grep -- "^${flag} - " | grep -Fq -- '[' ; then
 		count=2
 		while true; do
-			extra="$( echo "${line}" | awk -F'[' "{ print \$${count} }" | cut -d']' -f 1 )"
+			extra="$( echo "${line}" | awk -F'[' "{print \$${count}}" | cut -d']' -f 1 )"
 			if [ -n "${extra:-}" ]; then
 				[ $(( debug )) -eq 1 ] && echo >&2 "DEBUG: Found extra flag '${extra}'"
 				flag="${flag}|${extra}"
@@ -51,7 +51,7 @@ while read -r line; do
 		unset extra count
 	fi
 	#echo "${flag:-}"
-	grep -- '^flags' /proc/cpuinfo | tail -n 1 | awk -F': ' '{ print $2 }' | grep -Eq -- "${flag}" && echo "${flag}" | cut -d'|' -f 1
+	grep -- '^flags' /proc/cpuinfo | tail -n 1 | awk -F': ' '{print $2}' | grep -Eq -- "${flag}" && echo "${flag}" | cut -d'|' -f 1
 done < "${def_repo_path}/profiles/desc/cpu_flags_${1:-"x86"}.desc"
 
 exit 0

common/fix-preserved-pkgs.sh
@@ -12,7 +12,7 @@ test -x gentoo-build-pkg.docker || exit 1
 
 list="$( emerge -p @preserved-rebuild --with-bdeps=n |
 	grep -w 'R' |
-	awk '{ print $4 }' |
+	awk '{print $4}' |
 	cut -d':' -f 1
 )"
 

common/ls-duplicate-images.sh
@@ -61,7 +61,7 @@ php_replacement_2='\1-\3'
 images="$(
 	eval "${_command} image list --noheading ${all:+" ${all}"}"		|
 		sed -r "s|${php_pattern_1}|${php_replacement_1}|"		|
-		awk '{ print $1 }'						|
+		awk '{print $1}'						|
 		grep -v '<none>'						|
 		sort								|
 		uniq -c								|

common/prune.sh
@@ -20,33 +20,33 @@ $_command container ps -a |
 	rev |
 	cut -d' ' -f 1 |
 	rev |
-	grep '^[a-z]\+_[a-z]\+$' |
+	grep -- '^[a-z]\+_[a-z]\+$' |
 	xargs -r $_command container rm --volumes
 
 # Remove images classed as 'dangling'...
 #
 $_command image ls --filter 'dangling=true' |
 	tail -n +2 |
-	awk '{ print $3 }' |
+	awk '{print $3}' |
 	xargs -r $_command image rm || :
 
 # Try to remove remaining untagged images...
 #
 $_command image ls |
-	grep '^<none>\s\+<none>' |
-	awk '{ print $3 }' |
+	grep -- '^<none>\s\+<none>' |
+	awk '{print $3}' |
 	xargs -r $_command image rm || :
 $_command image ls |
-	grep '^<none>\s\+<none>' |
-	awk '{ print $3 }' |
+	grep -- '^<none>\s\+<none>' |
+	awk '{print $3}' |
 	xargs -r buildah rmi || :
 
 # Forcing image removal leads to greater problems :(
 #
 #while $_command image ls | grep -q '^<none>\s\+<none>'; do
 #	$_command image ls |
-#		grep '^<none>\s\+<none>' |
-#		awk '{ print $3 }' |
+#		grep -- '^<none>\s\+<none>' |
+#		awk '{print $3}' |
 #		xargs -r $_command image rm -f || :
 #done
 
@@ -61,14 +61,14 @@ $_command image ls |
 		# other - as above - can lead to corruption of the internal image
 		# registry...
 #		$_command image ls |
-#			grep '^<none>\s\+<none>' |
-#			awk '{ print $3 }' |
+#			grep -- '^<none>\s\+<none>' |
+#			awk '{print $3}' |
 #			xargs -rn 1 --  $_command image rm ||
 #		$_command image ls |
-#			grep '^<none>\s\+<none>' |
-#			awk '{ print $3 }' |
+#			grep -- '^<none>\s\+<none>' |
+#			awk '{print $3}' |
 #			xargs -rn 1 -- $_command image rm -f
-		$_command image ls | grep '^<none>\s\+<none>' | awk '{ print $3 }' | xargs -r buildah rmi || :
+		$_command image ls | grep -- '^<none>\s\+<none>' | awk '{print $3}' | xargs -r buildah rmi || :
 	}
 #done
 

common/run.sh
@@ -834,7 +834,7 @@ _docker_resolve() {
 						)" )"
 				elif [[ -d /etc/portage/repos.conf ]]; then
 					repopaths="$( # <- Syntax
-							grep '^\s*location\s*=\s*' /etc/portage/repos.conf/*.conf |
+							grep -- '^\s*location\s*=\s*' /etc/portage/repos.conf/*.conf |
 								cut -d'=' -f 2- |
 								awk '{print $NF}'
 						)"
@@ -902,7 +902,7 @@ _docker_resolve() {
 								export LLVM_MAJOR=0 PV=0
 								export LLVM_SOABI="${LLVM_MAJOR}"
 								eval "$( # <- Syntax
-										grep 'SLOT=' \
+										grep -- 'SLOT=' \
 											"${repopath}/${cat}/${pkg}/${eb}"
 									)" 2>/dev/null
 								unset LLVM_SOABI PV LLVM_MAJOR
@@ -1064,7 +1064,7 @@ _docker_image_exists() {
 	# shellcheck disable=SC2086
 	docker ${DOCKER_VARS:-} image ls "${image}:${version}" |
 		grep -E -- "^(localhost/)?([^.]+\.)?${image}" |
-		awk '{ print $3 }'
+		awk '{print $3}'
 
 	return 0
 }  # _docker_image_exists
@@ -1219,8 +1219,8 @@ _docker_run() {
 		local name='' ext=''
 		name="$( # <- Syntax
 				docker image ls |
-					grep "${image/:/\\s*}" |
-					awk '{ print $1 ":" $2 }'
+					grep -- "${image/:/\\s*}" |
+					awk '{print $1 ":" $2}'
 			)" || :
 		[[ -n "${name:-}" ]] && name="${name#*"/"}"
 		case "${name}" in
@@ -1369,12 +1369,12 @@ _docker_run() {
 				# shellcheck disable=SC2004
 				eval swp=$(( ( $(
 					grep -m 1 'SwapTotal:' /proc/meminfo |
-						awk '{ print $2 }'
+						awk '{print $2}'
 				) + 16 ) / ${divider} ))
 				# shellcheck disable=SC2004
 				eval ram=$(( $(
 					grep -m 1 'MemTotal:' /proc/meminfo |
-						awk '{ print $2 }'
+						awk '{print $2}'
 				) / ${divider} ))
 				# shellcheck disable=SC2295
 				if (( ram < ${PODMAN_MEMORY_LIMIT%[${unit,,}${unit^^}]} )) ||
@@ -1497,7 +1497,7 @@ _docker_run() {
 			warn "Using hard-coded defaults on non-Gentoo host system ..."
 			default_repo_path='/var/db/repos/gentoo /var/db/repos/srcshelton'
 			default_distdir_path='/var/cache/portage/dist'
-			default_pkgdir_path="/var/cache/portage/pkg/${ARCH:-"${arch}"}/${PKGHOST:-"docker"}"
+			default_pkgdir_path="/var/cache/portage/pkg/${ARCH:-"${arch}"}/${PKGHOST:-"container"}"
 			if ! [[ -d /var/db/repos/gentoo || -L /var/db/repos/gentoo ]] &&
 					[[ -d /var/db/repo/gentoo || -L /var/db/repo/gentoo ]]
 			then
@@ -1593,14 +1593,14 @@ _docker_run() {
 				_docker_setup
 			fi
 
-			#ENV PKGDIR="${PKGCACHE:-"/var/cache/portage/pkg"}/${ARCH:-"amd64"}/${PKGHOST:-"docker"}"
+			#ENV PKGDIR="${PKGCACHE:-"/var/cache/portage/pkg"}/${ARCH:-"amd64"}/${PKGHOST:-"container"}"
 			#local PKGCACHE="${PKGCACHE:="/var/cache/portage/pkg"}"
-			#local PKGHOST="${PKGHOST:="docker"}"
+			#local PKGHOST="${PKGHOST:="container"}"
 			local PKGDIR="${PKGDIR:="${default_pkgdir_path:-"$( portageq pkgdir )"}"}"
 
 			# Allow use of 'ARCH' variable as an override...
 			print "Using architecture '${ARCH:-"${arch}"}' ..."
-			mountpoints["${PKGDIR}"]="/var/cache/portage/pkg/${ARCH:-"${arch}"}/${PKGHOST:-"docker"}"
+			mountpoints["${PKGDIR}"]="/var/cache/portage/pkg/${ARCH:-"${arch}"}/${PKGHOST:-"container"}"
 			unset PKGDIR
 		fi
 		mountpointsro['/etc/portage/repos.conf']='/etc/portage/repos.conf.host'
@@ -1860,7 +1860,7 @@ _docker_run() {
 							grep -B 1 -- "\s${name:-"${container_name}"}$" |
 							grep -E '^[[:xdigit:]]{12}\s' |
 							tail -n 1 |
-							awk '{ print $1 }'
+							awk '{print $1}'
 				)" &&
 			[[ -n "${dr_id:-}" ]]
 	then
@@ -1941,7 +1941,7 @@ _docker_prune() {
 	# shellcheck disable=SC2031,SC2086
 	docker ${DOCKER_VARS:-} image ls |
 		grep -- '^<none>\s\+<none>' |
-		awk '{ print $3 }' |
+		awk '{print $3}' |
 		xargs -r /bin/sh -c docker ${DOCKER_VARS:-} image rm
 	trap - INT
 

common/vars.sh
@@ -66,7 +66,7 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 			echo "${_output}" |
 				grep -E -- '(graphRoot|Docker Root Dir):' |
 				cut -d':' -f 2- |
-				awk '{ print $1 }'
+				awk '{print $1}'
 		)" || :
 
 	# Optional override to specify alternative build-only temporary
@@ -535,7 +535,13 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 				# so let's see whether this is enough of a differentiator
 				# before having to parse the output of 'lscpu'...
 				#
-				case "$( grep 'CPU revision' /proc/cpuinfo | sort | uniq | cut -d':' -f 2 | awk '{print $1}' )" in
+				case "$( # <- Syntax
+							grep -- 'CPU revision' /proc/cpuinfo |
+								sort |
+								uniq |
+								cut -d':' -f 2 |
+								awk '{print $1}'
+						)" in
 					'0')
 						# "Grace" GH200 (via qemu)
 						use_cpu_flags='edsp neon thumb vfp vfpv3 vfpv4 vfp-d32 aes sha1 sha2 crc32 sm4 asimddp sve i8mm v4 v5 v6 v7 v8 thumb2' # v9
@@ -557,13 +563,13 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 				vendor="$( # <- Syntax
 					grep -- '^vendor_id' /proc/cpuinfo |
 						tail -n 1 |
-						awk -F': ' '{ print $2 }'
+						awk -F': ' '{print $2}'
 				)" || :
 				if [ -z "${vendor:-}" ]; then
 					vendor="$( # <- Syntax
 						grep -- '^CPU implementer' /proc/cpuinfo |
 							tail -n 1 |
-							awk -F': ' '{ print $2 }'
+							awk -F': ' '{print $2}'
 					)"
 				fi
 				if [ -r /proc/cpuinfo ]; then
@@ -580,17 +586,26 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 
 								# FIXME: Hard-coded /var/db/repo/gentoo...
 								while read -r line; do
-									echo "${line}" | grep -q -- ' - ' || continue
+									echo "${line}" |
+											grep -q -- ' - ' ||
+										continue
 
-									flag="$( echo "${line}" | awk -F' - ' '{ print $1 }' )"
-									if echo "${line}" | grep "^${flag} - " | grep -Fq -- '[' ; then
+									flag="$( # <- Syntax
+											echo "${line}" |
+												awk -F' - ' '{print $1}'
+										)"
+									if echo "${line}" |
+											grep -- "^${flag} - " |
+											grep -Fq -- '['
+									then
 										count=2
 										while true; do
 											extra="$( # <- Syntax
-												echo "${line}" |
-													awk -F'[' "{ print \$${count} }" |
-													cut -d']' -f 1
-											)"
+													echo "${line}" |
+														awk -F'[' \
+															"{print \$${count}}" |
+														cut -d']' -f 1
+												)"
 											if [ -n "${extra:-}" ]; then
 												flag="${flag}|${extra}"
 											else
@@ -601,23 +616,28 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 										unset extra count
 									fi
 									use_cpu_flags="${use_cpu_flags:-}$( # <- Syntax
-										grep -E -- '^(Features|flags)' /proc/cpuinfo |
-												tail -n 1 |
-												awk -F': ' '{ print $2 }' |
-												grep -Eq -- "${flag}" &&
-											echo " ${flag}" | cut -d'|' -f 1
-									)"
+											grep -E -- '^(Features|flags)' \
+														/proc/cpuinfo |
+													tail -n 1 |
+													awk -F': ' '{print $2}' |
+													grep -Eq -- "${flag}" &&
+												echo " ${flag}" |
+													cut -d'|' -f 1
+										)"
 								done < /var/db/repo/gentoo/profiles/desc/cpu_flags_x86.desc
 
 								use_cpu_flags="${use_cpu_flags#" "}"
-								echo >&2 "Auto-discovered CPU feature-flags '${use_cpu_flags}'"
+								echo >&2 "Auto-discovered CPU feature-flags" \
+									"'${use_cpu_flags}'"
 							fi
 							;;
 					esac
 				fi
 
 				if [ -z "${use_cpu_flags:-}" ]; then
-					echo >&2 "Unknown CPU '${description}' and 'cpuid2cpuflags' not installed - not enabling model-specific CPU flags"
+					echo >&2 "Unknown CPU '${description}' and" \
+						"'cpuid2cpuflags' not installed - not enabling" \
+						"model-specific CPU flags"
 				fi
 				;;
 		esac
@@ -698,7 +718,10 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 	#
 	case "$( uname -m )" in
 		x86_64|i686)
-			: $(( memtotal = $( grep -m 1 'MemTotal:' /proc/meminfo | awk '{ print $2 }' ) / 1024 / 1024 ))
+			: $(( memtotal = $( # <- Syntax
+					grep -m 1 'MemTotal:' /proc/meminfo |
+						awk '{print $2}'
+				) / 1024 / 1024 ))
 			# memtotal is rounded-down, so 4GB systems have a memtotal of 3...
 			if [ $(( memtotal )) -ge 4 ]; then
 				# Enable pypy support for Portage accleration of ~35%!
@@ -754,10 +777,10 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 	unset load jobs
 
 	# Allow a separate image directory for persistent images...
-	#store="$( $_command system info | grep -F 'overlay.imagestore:' | cut -d':' -f 2- | awk '{ print $1 }' )"
+	#store="$( $_command system info | grep -F 'overlay.imagestore:' | cut -d':' -f 2- | awk '{print $1}' )"
 	#if [ -n "${store}" ]; then
 	#	export IMAGE_ROOT="${store}"
-	#	store="$( $_command system info | grep 'graphRoot:' | cut -d':' -f 2- | awk '{ print $1 }' )"
+	#	store="$( $_command system info | grep -- 'graphRoot:' | cut -d':' -f 2- | awk '{print $1}' )"
 	#	if [ -n "${store}" ]; then
 	#		export GRAPH_ROOT="${store}"
 	#	fi

gentoo-base/Dockerfile.env
@@ -151,10 +151,10 @@ ENV use_essential="${SET_USE_ESSENTIAL}" \
 
 # Set filesystem locations ...
 #
-ARG PKGHOST="docker"
+ARG PKGHOST="container"
 ARG PKGCACHE="/var/cache/portage/pkg"
 
-ENV PKGDIR="${PKGCACHE:-/var/cache/portage/pkg}/${ARCH}/${PKGHOST:-docker}" \
+ENV PKGDIR="${PKGCACHE:-/var/cache/portage/pkg}/${ARCH}/${PKGHOST:-container}" \
     DISTDIR="/var/cache/portage/dist" \
     PORTAGE_LOGDIR="/var/log/portage" \
     PORTAGE_TMPDIR="/var/tmp" \

commit a4b7e3363e8231601c092e836636a6eba75fc688

Fix 'grep' invocation broken by 814e3ed39263bb5d574c9a40725fd174fb88ea86
Signed-off-by: Stuart Shelton <stuart@shelton.me>

common/ls-old-images.sh
@@ -49,7 +49,7 @@ echo "${images}" |
 	grep --colour=always '^localhost/gentoo-build.*$'
 
 echo "${images}" |
-	grep -- '^localhost/gentoo-build' -A "${lines:-"100"}" |
+	grep -A "${lines:-"100"}" -- '^localhost/gentoo-build' |
 	grep -v -e '^localhost/gentoo-\(build\|base\|init\|stage3\|env\)' -e '^docker.io/gentoo/stage3' |
 	grep --colour=never '^localhost/\(service\|sys-kernel\.\)'
 