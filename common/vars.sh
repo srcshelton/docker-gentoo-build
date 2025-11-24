e23bde5e6a72677cf6717052c7ce4d98ae722ef1
Stuart Shelton <stuart@shelton.me>
2025-11-24 13:50:12 +0000

    Fix Radxa Orion O6 detection between vendor and mainline kernels
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- common/vars.sh
+++ common/vars.sh
@@ -522,7 +522,9 @@ if [ -z "${__COMMON_VARS_INCLUDED:-}" ]; then
 			# of the big cores...
 			rust_target_opts="-C target-cpu=${target_cpu#*"."}" ;;
 
-		*': 0xd81'|*': Radxa Orion O6'*|*': CIX P1 CD8180')
+		*': 0xd81'|*': Radxa Orion O6'*|*': CIX P1 CD8180'|*': SKY1'*)
+			# N.B. In the default CIX patches, 'SKY1' is followed by whitespace
+			#
 			# ARMv9, big.LITTLE
 			# Features: fp asimd evtstrm aes pmull sha1 sha2 crc32 atomics
 			#           fphp asimdhp cpuid asimdrdm jscvt fcma lrcpc dcpop
